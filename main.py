959c71f50a86bc54862f64e32e833fc2c322557b
An Trieu <trieuhoangandemo@gmail.com>
2021-03-25 10:58:13 +0700

    seprate 2 kind of training and update gitignore

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from train import trainEpoch,trainIters,train
 import gensim
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if __name__=='__main__':
+def normal_train():
     input_size = 100
     hidden_size = 100
     p_dropout = 0.01
@@ -34,3 +34,27 @@ if __name__=='__main__':
     epoch = 15
     save_path = 'training'
     trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,epoch,0,0,save_path)
+def train_from_checkpoint():
+    path = 'training/checkpoint.pt'
+    checkpoint = torch.load(path)
+    input_size = 100
+    hidden_size = 100
+    p_dropout = 0.01
+    max_length = 870
+    enc = Tree2SeqEncoder(input_size,hidden_size,max_length,p_dropout,path_to_file_vi).to(device)
+    dec = Decoder(input_size,hidden_size,max_length,path_to_file_en,hidden_size,len(en_model.vocab)).to(device)
+    enc.load_state_dict(checkpoint['enc_state_dict'])
+    dec.load_state_dict(checkpoint['dec_state_dict'])
+    last_epoch = checkpoint['epoch']
+    last_iter = checkpoint['iter']
+    if iter == 0:
+    last_epoch = last_epoch + 1
+    input_data_path = '../DataForNMT/2013/train-en-vi/train.vi'
+    target_data_path = '../DataForNMT/2013/train-en-vi/train.en'
+    input_forest_path = '../DataForNMT/2013/train_phonlp_token_list.txt'
+    epoch = 17
+
+    save_path = '../models/NMTmodels/training'
+    trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,epoch,last_epoch,last_iter,save_path)
+if __name__=='__main__':
+    
