4d72875fcdb4a5107ed0c3544630c26c87f2c799
An Trieu <trieuhoangandemo@gmail.com>
2021-05-19 08:24:56 +0700

    add evaluate

--- main.py
+++ main.py
@@ -15,14 +15,10 @@ from train import trainEpoch,trainIters,train
 import gensim
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def normal_train(enc,dec):
-    input_data_path = 'data/train.vi'
-    target_data_path = 'data/train.en'
-    input_forest_path = 'data/tree_train.txt'
-    epoch = 15
+def normal_train(enc,dec,args):
     save_path = 'models/checkpoint'
-    trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,epoch,0,0,save_path)
-def train_from_checkpoint(enc,dec):
+    trainEpoch(enc,dec,args,0,0,save_path)
+def train_from_checkpoint(enc,dec,args):
     path = 'models/checkpoint/checkpoint.pt'
     checkpoint = torch.load(path)
     enc.load_state_dict(checkpoint['enc_state_dict'])
@@ -31,12 +27,8 @@ def train_from_checkpoint(enc,dec):
     last_iter = checkpoint['iter']
     if iter == 0:
         last_epoch = last_epoch + 1
-    input_data_path = 'data/train.vi'
-    target_data_path = 'data/train.en'
-    input_forest_path = 'data/tree_train.txt'
-    epoch = 15
     save_path = 'models/checkpoint'
-    trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,epoch,last_epoch,last_iter,save_path)
+    trainEpoch(enc,dec,args,last_epoch,last_iter,save_path)
 if __name__=='__main__':
     input_size = 100
     hidden_size = 100
@@ -44,8 +36,23 @@ if __name__=='__main__':
     max_length = 870
     path_to_file_vi = 'models/language_models/vi_model.bin'
     path_to_file_en = 'models/language_models/en_model.bin'
+    input_data_path = 'data/train.vi'
+    target_data_path = 'data/train.en'
+    input_forest_path = 'data/tree_train.txt'
+    input_valid_path = 'data/valid.vi'
+    target_data_path =  'data/valid.en'
+    valid_forest_path = 'data/valid_tree.txt'
+    epoch = 15
     en_model = gensim.models.KeyedVectors.load_word2vec_format(path_to_file_en,binary=True)
     vi_model = gensim.models.KeyedVectors.load_word2vec_format(path_to_file_vi,binary=True)
     enc = Tree2SeqEncoder(input_size,hidden_size,max_length,p_dropout,path_to_file_vi).to(device)
     dec = NewDecoder(input_size,hidden_size,max_length,path_to_file_en,hidden_size,len(en_model.vocab)).to(device)
-    train_from_checkpoint(enc,dec)
+    args = {"input_data_path":input_data_path,
+            "target_data_path":target_data_path,
+            "input_forest_path":input_forest_path,
+            "epoch":epoch,
+            "input_valid_path":input_valid_path,
+            "target_valid_path":target_valid_path,
+            "valid_forest_path":valid_forest_path
+            }
+    train_from_checkpoint(enc,dec,args)
--- train.py
+++ train.py
@@ -86,24 +86,25 @@ def train(input_tensor, target_tensor, input_forest ,encoder, decoder, encoder_o
             loss += criterion(decoder_output, target_tensor[:,di])
             tanh_hidden = decoder_tanh_hidden
             if check_end(decoder_input,batch_size):
-              # print(decoder_input)
+              target_length = di
               break
             decoder_input = target_tensor[:,di]  # Teacher forcing
             
     else:
         # Without teacher forcing: use its own predictions as the next input
         for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention,decoder_tree_attention = decoder(
-                decoder_input, decoder_hidden, encoder_seq_output.transpose(0,1),encoder_tree_output.transpose(0,1))
+            decoder_output, decoder_hidden,decoder_tanh_hidden, decoder_attention,c = decoder(
+                decoder_input, decoder_hidden,tanh_hidden ,encoder_seq_output.transpose(0,1),encoder_tree_output.transpose(0,1),numNode,c)
             '''
               decoder is in shape (B,H)
               mean first word of each sentence.
             '''
             loss += criterion(decoder_output, target_tensor[:,di])
-            decoder_input = torch.Tensor( target_tensor[:,di])
-            # topv, topi = decoder_output.topk(1)
-
-            # decoder_input = topi.squeeze().detach()  # detach from history as input
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+            if check_end(decoder_input,batch_size):
+              target_length = di
+              break
             # if decoder_input.item() == EOS_token:
             #     break
 
@@ -111,7 +112,7 @@ def train(input_tensor, target_tensor, input_forest ,encoder, decoder, encoder_o
     encoder_optimizer.step()
     decoder_optimizer.step()
 
-    return loss.item() / batch_size
+    return loss.item() / (target_length*batch)
 
 
 '''
@@ -126,7 +127,8 @@ def trainIters(encoder, decoder, input_sentence,input_tokenlist,target_sentence,
     print_loss_total = 0  # Reset every print_every
     plot_loss_total = 0  # Reset every plot_every
 
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.NLLLoss()
     # criterion = nn.MSELoss()
 
     total_exp = len(input_sentence)
@@ -173,10 +175,14 @@ def trainIters(encoder, decoder, input_sentence,input_tokenlist,target_sentence,
     return print_loss_total
 
 
-def trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,num_epoch,last_epoch,last_iter,save_path,learning_rate=0.02):
+def trainEpoch(encoder,dec,args,last_epoch,last_iter,save_path,learning_rate=0.02):
   import os
-  encoder_optimizer = optim.SGD(enc.parameters(), lr=learning_rate)
-  decoder_optimizer = optim.SGD(dec.parameters(), lr=learning_rate)
+  encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+  decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+  input_data_path = args['input_data_path']
+  target_data_path = args['target_data_path']
+  input_forest_path = args['input_forest_path']
+  num_epoch = args['epoch']
   for epoch in range(last_epoch,num_epoch):
     '''
       load data from file each time begin a new epoch
@@ -184,6 +190,7 @@ def trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,num_ep
     epoch_path = '{}/checkpoint.pt'.format(save_path)
     # if os.path.exists(epoch_dir)== False:
     #   os.mkdir(epoch_dir)
+    
     raw_input_text = open(input_data_path, 'rb').read().decode(encoding='utf-8')
     raw_target_text = open(target_data_path, 'rb').read().decode(encoding='utf-8')
 
@@ -196,19 +203,74 @@ def trainEpoch(enc,dec,input_data_path,target_data_path,input_forest_path,num_ep
     input_sent = preprocess_batch(input_sentences[:130000])
     target_sent = preprocessing_without_start(target_sentences[:130000])
     lst = load_simple_token_list_from_file(input_forest_path)
-
+    
     batch_size = 16
     max_length = 870
-    loss = trainIters(enc, dec,input_sent,lst[:130000],target_sent, batch_size,vi_model,en_model,max_length,save_path,epoch,last_iter,encoder_optimizer,decoder_optimizer)
+    loss = trainIters(encoder, decoder,input_sent,lst[:130000],target_sent, batch_size,vi_model,en_model,max_length,save_path,epoch,last_iter,encoder_optimizer,decoder_optimizer)
     print('finish epoch {} - loss {}'.format(epoch+1,loss))
     # spath = '{}/epoch_{}.pt'.format(epoch_dir,epoch)
     torch.save({
             'epoch': epoch,
             'iter':0,
-            'enc_state_dict': enc.state_dict(),
-            'dec_state_dict': dec.state_dict(),
+            'enc_state_dict': encoder.state_dict(),
+            'dec_state_dict': decoder.state_dict(),
             'encoder_optimizer': encoder_optimizer.state_dict(),
             'decoder_optimizer': decoder_optimizer.state_dict(),
             'loss': loss,
             }, epoch_path)
     last_iter = 0
+  evaluate(encoder,decoder,args,vi_model,en_model)
+def evaluate(encoder,decoder,args,input_model,target_model):
+  input_valid_path = args['input_valid_path']
+  target_valid_path = args['target_valid_path']
+  valid_forest_path = args['valid_forest_path']
+  raw_input_text = open(input_valid_path, 'rb').read().decode(encoding='utf-8')
+  raw_target_text = open(target_valid_path, 'rb').read().decode(encoding='utf-8')
+  input_sentences = []
+  target_sentences = []
+  input_sentences.extend(raw_input_text.split('\n'))
+  target_sentences.extend(raw_target_text.split('\n'))
+  input_sent = preprocess_batch(input_sentences)
+  target_sent = preprocessing_without_start(target_sentences)
+
+  lst = load_simple_token_list_from_file(valid_forest_path)
+  input_tensor,in_lengths = tensorFromSentence(model=input_model,sentences=input_batch,MAX_SEQUENCE_LENGTH=MAX_LENGTH)
+  target_tensor,tar_lengths = tensorFromSentence(model=target_model,sentences=target_batch,MAX_SEQUENCE_LENGTH=MAX_LENGTH)
+  input_forest = make_forest_from_token_list(forest_batch,input_model)
+  numExample = len(input_forest)
+  totalLoss = 0
+  criterion = nn.NLLLoss()
+  for i in range(0,numExample):
+    input_batch = [input_tensor[i]]
+    target_batch = [target_tensor[i]]
+    forest_batch = [input_forest[i]]
+    target_length = 700
+    loss = 0
+    numNode,encoder_tree_output,encoder_seq_output,encoder_tree_hc,encoder_seq_hc = encoder(input_batch,forest_batch)
+    maxNode = numNode[0]
+    maxNode += 1
+    tanh_hidden = decoder.init_new_hidden()
+    word_input = []
+    word_input.append(target_model.vocab['<start>'].index)
+    decoder_input = torch.tensor(word_input, device=device)
+    last_seq_hidden = encoder_seq_output[:,maxNode].unsqueeze(0)
+    decoder_hidden = decoder.get_first_hidden(encoder_tree_hc[0],last_seq_hidden,encoder_tree_hc[1],encoder_seq_hc[1])
+    use_teacher_forcing = True
+    c = torch.zeros(batch_size,decoder.hidden_size).to(device)
+    for di in range(target_length):
+      decoder_output, decoder_hidden,decoder_tanh_hidden, decoder_attention,c = decoder(
+          decoder_input, decoder_hidden,tanh_hidden ,encoder_seq_output.transpose(0,1),encoder_tree_output.transpose(0,1),numNode,c)
+      '''
+        decoder is in shape (B,H)
+        mean first word of each sentence.
+      '''
+      loss += criterion(decoder_output, target_tensor[:,di])
+      topv, topi = decoder_output.topk(1)
+      decoder_input = topi.squeeze().detach()  # detach from history as input
+      if check_end(decoder_input,batch_size):
+        target_length = di
+        break
+    loss = loss/target_length
+    totalLoss += loss
+  print("validation step: Loss :",totalLoss)
+    
